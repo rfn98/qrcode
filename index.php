a9d04b1f4570f5cb8625864452b1c983145c4a41
rfn98 <rifanajieagung48@gmail.com>
2022-04-15 18:49:24 +0700

    fitur scan qr code sederhana

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Instascan</title>
+  </head>
+  <body>
+    <video id="preview"></video>
+    <span id="qrcode"></span>
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
+    <script type="text/javascript">
+        let scanner = new Instascan.Scanner(
+            {
+                video: document.getElementById('preview')
+            }
+        );
+        scanner.addListener('scan', function(content) {
+            alert('Escaneou o conteudo: ' + content);
+            window.open(content, "_blank");
+        });
+        Instascan.Camera.getCameras().then(cameras => 
+        {
+            if(cameras.length > 0){
+                scanner.start(cameras[0]);
+            } else {
+                console.error("Não existe câmera no dispositivo!");
+            }
+        });
+    </script>
+
+ </body>
+</html>
