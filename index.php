01dd1b45fb3a2462e7d161879d258e7eadb23592
rfn98 <rifanajieagung48@gmail.com>
2022-04-15 19:27:06 +0700

    fix scan qrcode for mobile

--- index.php
+++ index.php
@@ -19,7 +19,9 @@
         });
         Instascan.Camera.getCameras().then(cameras => 
         {
-            if(cameras.length > 0){
+            if(cameras.length > 1){
+                scanner.start(cameras[1]);
+            } else if(cameras.length > 0){
                 scanner.start(cameras[0]);
             } else {
                 console.error("Não existe câmera no dispositivo!");
